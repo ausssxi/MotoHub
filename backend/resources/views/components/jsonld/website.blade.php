4d7cdd3e40a64e83035b01a0f9f2720f65ce7709
ausssxi <ausssxi@kb3.so-net.ne.jp>
2026-08-08 18:22:01 +0900

    fix(seo): サイト共通のJSON-LDが壊れていた問題を修正
    components/jsonld/website.blade.php で配列リテラルを {!! json_encode([...]) !!} の
    式の中に直接書いていたため、Blade がその内側もディレクティブとして走査し、
    '@context' が Laravel 11 の @context ディレクティブとしてコンパイルされていた。
    結果、出力JSONのキーがコンパイル済みPHPのソースに化け、全ページの
    Organization / WebSite 構造化データが解析不能になっていた。
    
    配列を @php ブロックで組み立て、script 内では変数を渡すだけにする。
    リポジトリ内の他のJSON-LD出力箇所（roadside_station など）は全てこの流儀で、
    このファイルだけが例外だった。JSONの内容・キー・値・順序・
    オプションフラグは変更していない。

--- backend/resources/views/components/jsonld/website.blade.php
+++ backend/resources/views/components/jsonld/website.blade.php
@@ -1,56 +1,66 @@
 {{--
     構造化データ: Organization + WebSite
     配置場所: layout.blade.php の </head> 直前
-    
-    ※Bladeの誤作動を防ぐため、PHP配列からJSONに変換して出力しています。
+
+    ※ JSON-LD の配列は下の PHP ブロックで組み立て、script 内では変数を json_encode するだけにしている。
+      こうしないと「@ 始まりの JSON-LD キー」が Blade ディレクティブとして解釈され、構造化データが壊れる。
+      具体的な理由は下の PHP ブロック内コメントを参照。
 --}}
-<script type="application/ld+json">
-{!! json_encode([
-    '@context' => 'https://schema.org',
-    '@graph' => [
-        [
-            '@type' => 'Organization',
-            '@id' => url('/') . '/#organization',
-            'name' => 'MotoHub',
-            'url' => url('/'),
-            'logo' => [
-                '@type' => 'ImageObject',
-                'url' => asset('images/twitter_card.png'),
-                'width' => 512,
-                'height' => 512
-            ],
-            'description' => '日本最大級のバイク検索・比較プラットフォーム。GooBike、BDS、Webikeから一括検索！',
-            'founder' => [
-                '@type' => 'Person',
-                'name' => '内田厚'
-            ],
-            'sameAs' => [
-                'https://x.com/motohub_jp',
-                'https://www.instagram.com/motohub.jp',
-                'https://www.youtube.com/@motohub_jp',
-                'https://www.tiktok.com/@motohub10',
-                'https://note.com/motohub',
-                'https://qiita.com/ausssxi0'
-            ]
-        ],
-        [
-            '@type' => 'WebSite',
-            '@id' => url('/') . '/#website',
-            'url' => url('/'),
-            'name' => 'MotoHub',
-            'description' => '日本最大級のバイク検索・比較プラットフォーム',
-            'publisher' => [
-                '@id' => url('/') . '/#organization'
-            ],
-            'potentialAction' => [
-                '@type' => 'SearchAction',
-                'target' => [
-                    '@type' => 'EntryPoint',
-                    'urlTemplate' => url('/bikes/search') . '?keyword={search_term_string}'
+@php
+    // 出力側で配列リテラルを直接 json_encode すると、生出力構文 {!! ... !!} の式の内側も
+    // Blade がディレクティブを走査するため、'@context' が Laravel の @context ディレクティブとして
+    // コンパイルされ、出力JSONのキーがコンパイル済みPHPに化ける
+    //（本番トップの Organization / WebSite が解析不能になっていた）。
+    // 対策として、ここで変数に組み立ててから、出力側は変数を渡すだけにする。
+    $ldWebsite = [
+        '@context' => 'https://schema.org',
+        '@graph' => [
+            [
+                '@type' => 'Organization',
+                '@id' => url('/') . '/#organization',
+                'name' => 'MotoHub',
+                'url' => url('/'),
+                'logo' => [
+                    '@type' => 'ImageObject',
+                    'url' => asset('images/twitter_card.png'),
+                    'width' => 512,
+                    'height' => 512
                 ],
-                'query-input' => 'required name=search_term_string'
+                'description' => '日本最大級のバイク検索・比較プラットフォーム。GooBike、BDS、Webikeから一括検索！',
+                'founder' => [
+                    '@type' => 'Person',
+                    'name' => '内田厚'
+                ],
+                'sameAs' => [
+                    'https://x.com/motohub_jp',
+                    'https://www.instagram.com/motohub.jp',
+                    'https://www.youtube.com/@motohub_jp',
+                    'https://www.tiktok.com/@motohub10',
+                    'https://note.com/motohub',
+                    'https://qiita.com/ausssxi0'
+                ]
+            ],
+            [
+                '@type' => 'WebSite',
+                '@id' => url('/') . '/#website',
+                'url' => url('/'),
+                'name' => 'MotoHub',
+                'description' => '日本最大級のバイク検索・比較プラットフォーム',
+                'publisher' => [
+                    '@id' => url('/') . '/#organization'
+                ],
+                'potentialAction' => [
+                    '@type' => 'SearchAction',
+                    'target' => [
+                        '@type' => 'EntryPoint',
+                        'urlTemplate' => url('/bikes/search') . '?keyword={search_term_string}'
+                    ],
+                    'query-input' => 'required name=search_term_string'
+                ]
             ]
         ]
-    ]
-], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}
+    ];
+@endphp
+<script type="application/ld+json">
+{!! json_encode($ldWebsite, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}
 </script>
